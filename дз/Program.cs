b753c08ad80a7ada551cd41abd9e861527b79b61
Anastasia <bogdanovanastja@yandex.ru>
2022-10-19 02:46:34 +0300

    g

--- дз/Program.cs
+++ дз/Program.cs
@@ -1,1 +1,99 @@
-﻿
+﻿/* // Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int [] Fillsmth ()
+
+{
+    Console.WriteLine("Cколько чисел будем вводить?");
+    int M = int.Parse(Console.ReadLine());
+    int [] a = new int [M];
+    Console.Write("Введите число ");
+    for (int i = 0; i <M; i++)
+    {
+        a[i] = int.Parse(Console.ReadLine());    
+    }
+    return a;
+}
+
+void Countpos (int [] arr)
+{
+   int n = 0;
+   for (int i = 0; i <arr.Length; i++)
+    {
+    if (arr[i] > 0) n++;
+    Console.Write(arr[i] + " ");
+    }
+    
+    Console.WriteLine($"Количество положительных чисел равно {n}"); 
+}
+int [] aaa = Fillsmth ();
+Countpos (aaa); */
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+// Console.WriteLine("Введите k и b для первой прямой");
+// int k1 = int.Parse(Console.ReadLine());
+// int b1 = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите k и b для второй прямой");
+// int k2 = int.Parse(Console.ReadLine());
+// int b2 = int.Parse(Console.ReadLine());
+
+// if (k1 == k2) 
+// {
+//     if (b1==b2) 
+//     Console.WriteLine("Они совпадают"); 
+//     else Console.WriteLine("Они параллельны"); 
+// }
+// double x01 = b2-b1;
+// double x02 = k1-k2;
+// double x03 = x01/x02;
+// //double x0 = (b2-b1)/(k1-k2); // я хз что не так, но считает неправильно. 
+// double y0 = x03*k1+b1;
+// double y1 = x03*k2+b2;
+
+// Console.WriteLine($"Точка пересечения: х = {x03}, у = {y0}");
+
+// Дано действительное положительное число a и целое неотрицательное число n. Вычислите an не используя циклы,
+// возведение в степень через ** и функцию math.pow(), а используя рекуррентное соотношение an=a⋅an-1.
+// Решение оформите в виде функции power(a, n).
+
+
+/* //  Заполнить двумерный массив следующим образом. Вводится число n.
+
+int n = int.Parse(Console.ReadLine());
+int [,] m = new int [n,n];
+
+for (int i = 0; i<m.GetLength(0); i++)
+{
+        for (int j = 0; j<m.GetLength(1); j++)
+    {
+        
+        if (i>=j) m[i,j] = j+1;
+        else   m[i,j] = i+1;     
+        Console.Write(m[i,j] + " ");
+    }
+    Console.WriteLine();
+} */
+
+ /* // Дано действительное положительное число a и целое неотрицательное число n. Вычислите an не используя циклы, 
+// возведение в степень через ** и функцию math.pow(), а используя рекуррентное соотношение an=a⋅an-1.
+// Решение оформите в виде функции power(a, n).
+
+
+double power( double a, int n )
+{
+  
+if(n == 1) return a;
+else return a * power( a, n-1 );
+}
+
+Console.WriteLine("Введите число и степень, в которую его нужно возвести");
+double num = int.Parse(Console.ReadLine());
+int step = int.Parse(Console.ReadLine());
+if (step <0) Console.WriteLine("Упс");
+double dd = power(num, step);
+Console.WriteLine(dd); */
